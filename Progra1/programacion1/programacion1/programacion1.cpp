76743aa5ae1998524886170a431c19023179209b
Andrey Morales Mendoza <andrey.morales.mendoza@est.una.ac.cr>
2025-10-02 18:21:36 -0600

    programacion1

---
+++ Progra1/programacion1/programacion1/programacion1.cpp
@@ -0,0 +1,10 @@
+// archivo en el main new repository github
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	cout << "Hello World!\n" << endl; // prints Hello World!, \n and endl are both used to create a new line
+	return 0; 
+}
