48440eae57d7c587459b0aedd43a05e1910666b0
AndreyEducativo <andrey.morales.mendoza@est.una.ac.cr>
2025-10-29 21:29:11 -0600

    revisar

--- Progra1/programacion1/programacion1/programacion1.cpp
+++ Progra1/programacion1/programacion1/programacion1.cpp
@@ -28,7 +28,7 @@ int main() {
 
         lista.Mostrar();
 
-        // SEGUNDO MÉTODO: Insertar entre dos nodos (no sirve)
+        // SEGUNDO MÉTODO: Insertar entre dos nodos (no sirve/revisar)
         cout << "\nDesea insertar un dato entre dos existentes? (s/n): ";
         cin >> respuesta;
 
